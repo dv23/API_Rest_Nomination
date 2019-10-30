9a6d82196c698096038ab35b259f4979c948fe89
Vincent <dubeaurepaire.v@gmail.com>
2019-10-30 09:42:36 +0100

    symony api contact entreprise

---
+++ src/AppBundle/Controller/ContactController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+/** classe Route pour annotation @Route **/
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use AppBundle\Entity\Contact;
+//use AppBundle\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+ use Symfony\Component\HttpFoundation\Request;
+//@Method("GET")
+//use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Contact controller.
+ * 
+ */
+class ContactController extends Controller
+{
+    /**
+     * Lists all contact entities.
+     * @Route("/list/contacts", name="contact_list")
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $contacts = $em->getRepository('AppBundle:Contact')->findAll();
+
+        return $this->render('contact/index.html.twig', array(
+            'contacts' => $contacts,
+        ));
+    }
+
+    /**
+     * Creates a new contact entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $contact = new Contact();
+        $form = $this->createForm('AppBundle\Form\ContactType', $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact->setEntreprise($contact->getEntreprise()->getId()); // integer
+
+            // Enregistrement des données dans la base
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($contact);
+            $em->flush();
+
+            return $this->redirectToRoute('contact_show', array('id' => $contact->getId()));
+        }
+
+        return $this->render('contact/new.html.twig', array(
+            'contact' => $contact,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a contact entity.
+     *
+     */
+    public function showAction(Contact $contact)
+    {
+        $deleteForm = $this->createDeleteForm($contact);
+
+        return $this->render('contact/show.html.twig', array(
+            'contact' => $contact,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing contact entity.
+     *
+     */
+    public function editAction(Request $request, Contact $contact)
+    {
+        $deleteForm = $this->createDeleteForm($contact);
+        $editForm = $this->createForm('AppBundle\Form\ContactType', $contact);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('contact_edit', array('id' => $contact->getId()));
+        }
+
+        return $this->render('contact/edit.html.twig', array(
+            'contact' => $contact,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a contact entity.
+     *
+     */
+    public function deleteAction(Request $request, Contact $contact)
+    {
+        $form = $this->createDeleteForm($contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($contact);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('contact_index');
+    }
+
+    /**
+     * Creates a form to delete a contact entity.
+     *
+     * @param Contact $contact The contact entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Contact $contact)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('contact_delete', array('id' => $contact->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/AppBundle/Entity/Contact.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Contact
+ *
+ * @ORM\Table(name="contact")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ContactRepository")
+ */
+class Contact
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, unique=true)
+     */
+    private $email;
+
+    /**
+     * @var int
+     ** Pour rendre le champ entrepise facultatif, il suffit de le notifier dans annotations
+     * @ORM\Column(name="entreprise", type="integer", nullable=true)
+     */
+    private $entreprise; 
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Contact
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Contact
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set entreprise
+     *
+     * @param string $entreprise
+     *
+     * @return Contact
+     */
+    public function setEntreprise($entreprise)
+    {
+        $this->entreprise = $entreprise;
+
+        return $this;
+    }
+
+    /**
+     * Get entreprise
+     *
+     * @return string
+     */
+    public function getEntreprise()
+    {
+        return $this->entreprise;
+    }
+}
